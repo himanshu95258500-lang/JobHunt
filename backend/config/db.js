64d480590e2f6fb0c4a04e25624519260940b5c2
JobHunt Developer <jobhunt-developer@users.noreply.github.com>
2026-09-07 10:35:14 +0530

    Configure database SSL for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,20 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
+const sslEnabled = process.env.DB_SSL === "true";
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+
+  ssl: sslEnabled
+    ? {
+        minVersion: "TLSv1.2",
+      }
+    : undefined,
 });
 
 db.connect((err) => {
