cb4c6e1cc3f6e2dd39b97ee3c4d4a77f6aeb8edc
JobHunt Developer <jobhunt-developer@users.noreply.github.com>
2026-09-07 11:03:46 +0530

    Use MySQL connection pool

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const sslEnabled = process.env.DB_SSL === "true";
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -15,15 +15,23 @@ const db = mysql.createConnection({
         minVersion: "TLSv1.2",
       }
     : undefined,
+
+  connectionLimit: 5,
+  maxIdle: 2,
+  idleTimeout: 60000,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 10000,
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
     console.error("MySQL connection failed:", err.message);
     return;
   }
 
   console.log("MySQL connected successfully!");
+
+  connection.release();
 });
 
 module.exports = db;
